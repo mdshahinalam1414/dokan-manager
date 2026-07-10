9e413ba8c1bd6e339344fd21961557c8206fe5d2
Md Shahin Alam <155907922+mdshahinalam1414@users.noreply.github.com>
2026-07-10 04:19:18 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// এখানে আপনার Google Apps Script Web App ডিপ্লয় করার পর যেই URL পাবেন সেটি বসান
+// (Deploy > New deployment > Web app > Deploy করার পর যেই লিংক দেখাবে)
+const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";

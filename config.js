6478a13280d9af6eeee2c7180c0fdeee0ff3c10d
Md Shahin Alam <155907922+mdshahinalam1414@users.noreply.github.com>
2026-07-10 22:39:10 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // এখানে আপনার Google Apps Script Web App ডিপ্লয় করার পর যেই URL পাবেন সেটি বসান
 // (Deploy > New deployment > Web app > Deploy করার পর যেই লিংক দেখাবে)
-const API_URL = "https://script.google.com/macros/s/AKfycbxT2l-N5z4d-YE6cPvWVfiaZNf2i_-ajGa6exkqI3xr0iUM2vJl5PBsRTMNWND74bUTSA/exec";
+const API_URL = "https://script.google.com/macros/library/d/1eJWllGbHliu3a2LRi2lD8iZcCbG_Kcwyc4dDPTLZT47glP9urHw9u3CK/1";
